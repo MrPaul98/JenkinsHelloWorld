97e8621d62c12512355e6d20b8d50b4036bd52b9
Sayandip Paul <68201896+MrPaul98@users.noreply.github.com>
2022-06-26 13:41:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("-----***** Sayandip Paul Hello World*****-----");
+		int a = 2;
+		int b = 3;
+		int c = a + b;
+		System.out.println("-----***** Sayandip Paul Lucky NUMBER*****-----" +  c);
 	}
 }
